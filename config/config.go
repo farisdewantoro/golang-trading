d72f64f1918d933bca2445a2ae0529a3a97abaa0
Faris Dewantoro <faris.dewantoro@efishery.com>
2025-07-08 08:45:44 +0700

    fix(env): config file

--- config/config.go
+++ config/config.go
@@ -2,114 +2,138 @@ package config
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Log         Logger         `mapstructure:"logger"`
-	DB          Database       `mapstructure:"database"`
-	API         API            `mapstructure:"api"`
-	Scheduler   Scheduler      `mapstructure:"scheduler"`
-	TradingView TradingView    `mapstructure:"tradingview"`
-	Cache       Cache          `mapstructure:"cache"`
-	Telegram    TelegramConfig `mapstructure:"telegram"`
+	Log         Logger
+	DB          Database
+	API         API
+	Scheduler   Scheduler
+	TradingView TradingView
+	Cache       Cache
+	Telegram    TelegramConfig
 }
 
 type Logger struct {
-	Level    string `mapstructure:"level"`
-	Encoding string `mapstructure:"encoding"`
+	Level    string
+	Encoding string
 }
 
 type Database struct {
-	Host            string `mapstructure:"host"`
-	Port            int    `mapstructure:"port"`
-	User            string `mapstructure:"user"`
-	Password        string `mapstructure:"password"`
-	DBName          string `mapstructure:"name"`
-	SSLMode         string `mapstructure:"ssl_mode"`
-	TimeZone        string `mapstructure:"time_zone"`
-	MaxIdleConns    int    `mapstructure:"max_idle_conns"`
-	MaxOpenConns    int    `mapstructure:"max_open_conns"`
-	ConnMaxLifetime string `mapstructure:"conn_max_lifetime"`
-	LogLevel        string `mapstructure:"log_level"`
+	Host            string
+	Port            int
+	User            string
+	Password        string
+	DBName          string
+	SSLMode         string
+	TimeZone        string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime string
+	LogLevel        string
 }
 
 type Scheduler struct {
-	MaxConcurrency  int           `mapstructure:"max_concurrency"`
-	TimeoutDuration time.Duration `mapstructure:"timeout_duration"`
+	MaxConcurrency  int
+	TimeoutDuration time.Duration
 }
 
 type API struct {
-	Port int `mapstructure:"port"`
+	Port int
 }
 
 type TradingView struct {
-	BaseURLScanner   string        `mapstructure:"base_url_scanner"`
-	BaseTimeout      time.Duration `mapstructure:"base_timeout"`
-	MaxRequestPerMin int           `mapstructure:"max_request_per_min"`
+	BaseURLScanner   string
+	BaseTimeout      time.Duration
+	MaxRequestPerMin int
 }
 
 type Cache struct {
-	DefaultExpiration time.Duration `mapstructure:"default_expiration"`
-	CleanupInterval   time.Duration `mapstructure:"cleanup_interval"`
+	DefaultExpiration time.Duration
+	CleanupInterval   time.Duration
 }
 
 type TelegramConfig struct {
-	BotToken                  string        `mapstructure:"bot_token"`
-	ChatID                    string        `mapstructure:"chat_id"`
-	WebhookURL                string        `mapstructure:"webhook_url"`
-	TimeoutDuration           time.Duration `mapstructure:"timeout_duration"`
-	TimeoutBuyListDuration    time.Duration `mapstructure:"timeout_buy_list_duration"`
-	MaxGlobalRequestPerSecond int           `mapstructure:"max_global_request_per_second"`
-	MaxUserRequestPerSecond   int           `mapstructure:"max_user_request_per_second"`
-	MaxEditMessagePerSecond   int           `mapstructure:"max_edit_message_per_second"`
-	RatelimitExpireDuration   time.Duration `mapstructure:"ratelimit_expire_duration"`
-	RateLimitCleanupDuration  time.Duration `mapstructure:"rate_limit_cleanup_duration"`
-	FeatureNewsMaxAgeInDays   int           `mapstructure:"feature_news_max_age_in_days"`
-	FeatureNewsLimitStockNews int           `mapstructure:"feature_news_limit_stock_news"`
-	MaxShowHistoryAnalysis    int           `mapstructure:"max_show_history_analysis"`
+	BotToken                  string
+	ChatID                    string
+	WebhookURL                string
+	TimeoutDuration           time.Duration
+	TimeoutBuyListDuration    time.Duration
+	MaxGlobalRequestPerSecond int
+	MaxUserRequestPerSecond   int
+	MaxEditMessagePerSecond   int
+	RatelimitExpireDuration   time.Duration
+	RateLimitCleanupDuration  time.Duration
+	FeatureNewsMaxAgeInDays   int
+	FeatureNewsLimitStockNews int
+	MaxShowHistoryAnalysis    int
 }
 
 func Load() (*Config, error) {
-	viper.SetConfigType("yaml")
+	// Load .env file. It's okay if it doesn't exist.
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Failed to load .env file %v\n", err)
+	}
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AddConfigPath(".")
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("No config file loaded:", err)
-	}
-
-	fmt.Println("Viper settings for environment variables:")
-
-	envVarsToCheck := []string{
-		"LOGGER_LEVEL",
-		"DATABASE_HOST",
-		"DATABASE_PORT",
-		"TELEGRAM_BOT_TOKEN", // Check a sensitive one too
-	}
-	for _, ev := range envVarsToCheck {
-		val, exists := os.LookupEnv(ev)
-		if exists {
-			fmt.Printf("  os.LookupEnv - %s: %s\n", ev, val)
-		} else {
-			fmt.Printf("  os.LookupEnv - %s: NOT FOUND\n", ev)
-		}
-	}
-
-	for _, key := range viper.AllKeys() {
-		if viper.IsSet(key) {
-			fmt.Printf("  Key: %s, Value: %v\n", key, viper.Get(key))
-		}
-	}
-
 	var cfg Config
-	if err := viper.Unmarshal(&cfg); err != nil {
-		return nil, err
+
+	cfg = Config{
+		Log: Logger{
+			Level:    viper.GetString("LOGGER_LEVEL"),
+			Encoding: viper.GetString("LOGGER_ENCODING"),
+		},
+		DB: Database{
+			Host:            viper.GetString("DATABASE_HOST"),
+			Port:            viper.GetInt("DATABASE_PORT"),
+			User:            viper.GetString("DATABASE_USER"),
+			Password:        viper.GetString("DATABASE_PASSWORD"),
+			DBName:          viper.GetString("DATABASE_NAME"),
+			SSLMode:         viper.GetString("DATABASE_SSL_MODE"),
+			TimeZone:        viper.GetString("DATABASE_TIMEZONE"),
+			MaxIdleConns:    viper.GetInt("DATABASE_MAX_IDLE_CONNS"),
+			MaxOpenConns:    viper.GetInt("DATABASE_MAX_OPEN_CONNS"),
+			ConnMaxLifetime: viper.GetString("DATABASE_CONN_MAX_LIFETIME"),
+			LogLevel:        viper.GetString("DATABASE_LOG_LEVEL"),
+		},
+		API: API{
+			Port: viper.GetInt("API_PORT"),
+		},
+		Scheduler: Scheduler{
+			MaxConcurrency:  viper.GetInt("SCHEDULER_MAX_CONCURRENCY"),
+			TimeoutDuration: viper.GetDuration("SCHEDULER_TIMEOUT_DURATION"),
+		},
+		TradingView: TradingView{
+			BaseURLScanner:   viper.GetString("TRADINGVIEW_BASE_URL_SCANNER"),
+			BaseTimeout:      viper.GetDuration("TRADINGVIEW_BASE_TIMEOUT"),
+			MaxRequestPerMin: viper.GetInt("TRADINGVIEW_MAX_REQUEST_PER_MIN"),
+		},
+		Cache: Cache{
+			DefaultExpiration: viper.GetDuration("CACHE_DEFAULT_EXPIRATION"),
+			CleanupInterval:   viper.GetDuration("CACHE_CLEANUP_INTERVAL"),
+		},
+		Telegram: TelegramConfig{
+			BotToken:                  viper.GetString("TELEGRAM_BOT_TOKEN"),
+			ChatID:                    viper.GetString("TELEGRAM_CHAT_ID"),
+			WebhookURL:                viper.GetString("TELEGRAM_WEBHOOK_URL"),
+			TimeoutDuration:           viper.GetDuration("TELEGRAM_TIMEOUT_DURATION"),
+			TimeoutBuyListDuration:    viper.GetDuration("TELEGRAM_TIMEOUT_BUY_LIST_DURATION"),
+			MaxGlobalRequestPerSecond: viper.GetInt("TELEGRAM_MAX_GLOBAL_REQUEST_PER_SECOND"),
+			MaxUserRequestPerSecond:   viper.GetInt("TELEGRAM_MAX_USER_REQUEST_PER_SECOND"),
+			MaxEditMessagePerSecond:   viper.GetInt("TELEGRAM_MAX_EDIT_MESSAGE_PER_SECOND"),
+			RatelimitExpireDuration:   viper.GetDuration("TELEGRAM_RATELIMIT_EXPIRE_DURATION"),
+			RateLimitCleanupDuration:  viper.GetDuration("TELEGRAM_RATELIMIT_CLEANUP_DURATION"),
+			FeatureNewsMaxAgeInDays:   viper.GetInt("TELEGRAM_FEATURE_NEWS_MAX_AGE_IN_DAYS"),
+			FeatureNewsLimitStockNews: viper.GetInt("TELEGRAM_FEATURE_NEWS_LIMIT_STOCK_NEWS"),
+			MaxShowHistoryAnalysis:    viper.GetInt("TELEGRAM_MAX_SHOW_HISTORY_ANALYSIS"),
+		},
 	}
 
 	return &cfg, nil
--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/go-playground/validator/v10 v10.27.0
 	github.com/go-resty/resty/v2 v2.16.5
 	github.com/golang-migrate/migrate/v4 v4.18.3
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/cobra v1.9.1
-	github.com/spf13/viper v1.20.1
+	github.com/spf13/viper v1.13.0
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/time v0.12.0
@@ -23,15 +24,15 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -39,19 +40,22 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/sagikazarmark/locafero v0.7.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.12.0 // indirect
-	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
@@ -61,6 +65,8 @@ require (
 	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
 )
