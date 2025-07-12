99c41b1b1ecb48ecd7d369abecc99aaee756c4a7
Faris Dewantoro <faris.dewantoro@efishery.com>
2025-07-12 15:28:18 +0700

    fix(yahoo): set header

--- internal/repository/yahoo_finance_repo.go
+++ internal/repository/yahoo_finance_repo.go
@@ -75,12 +75,21 @@ func (r *yahooFinanceRepository) Get(ctx context.Context, param dto.GetStockData
 	}
 
 	headers := map[string]string{
-		"User-Agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
-		"Accept":          "application/json, text/plain, */*",
-		"Accept-Language": "en-US,en;q=0.9",
-		"Accept-Encoding": "gzip, deflate, br",
-		"Connection":      "keep-alive",
-		"Referer":         "https://finance.yahoo.com/",
+		"User-Agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
+		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"Accept-Language":           "en-US,en;q=0.9",
+		"Accept-Encoding":           "gzip, deflate, br, zstd",
+		"Referer":                   "https://finance.yahoo.com/",
+		"Cache-Control":             "max-age=0",
+		"Priority":                  "u=0, i",
+		"Sec-Fetch-Dest":            "document",
+		"Sec-Fetch-Mode":            "navigate",
+		"Sec-Fetch-Site":            "none",
+		"Sec-Fetch-User":            "?1",
+		"Upgrade-Insecure-Requests": "1",
+		"Sec-Ch-UA":                 `"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"`,
+		"Sec-Ch-UA-Mobile":          "?0",
+		"Sec-Ch-UA-Platform":        "macOS",
 	}
 
 	var yahooResp dto.YahooFinanceResponse
